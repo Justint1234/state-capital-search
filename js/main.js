8706e4dba1beafb47cce64177b13cd57fc6675f6
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-07-28 16:23:40 -0400

    filtering through states

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+const searchStates = async searchText => {
+    const res = await fetch('../data/states.json');
+    const states = await res.json();
+
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex); 
+
+    });
+    console.log(matches); 
+};
+
+search.addEventListener('input', () => searchStates(search.value))
